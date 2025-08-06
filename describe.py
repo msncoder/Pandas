1a9b75e5d23e706311f48ed68eb2c06db4ab8e03
saad <msncoder1999@gmail.com>
2025-08-06 05:41:07 -0700

    add describe.py file

---
+++ describe.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+data = {
+    "name" : ["saad","fahad","samad","asad"],
+    "age" : [25,36,35,38],
+    "salary":[320,600,200,400],
+    "performance_score":[82,90,92,95]
+}
+
+df = pd.DataFrame(data)
+print("Simple data frame")
+print(df)
+print("Descriptive Data")
+print(df.describe())
